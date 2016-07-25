5a792201b52bc7e28fdd40662ec3ace5977f721f
Vinci <vinci.rufus@gmail.com>
2016-07-26 00:16:12 +0530

    cleanup

--- app/index.js
+++ app/index.js
@@ -1,27 +1,17 @@
 var SlackBot = require('slackbots');
- 
+
 // create a bot 
 var bot = new SlackBot({
-    token: 'xoxb-61448927794-42lYwtaIn8vS6kZRSCB3rZ7F', // Add a bot https://my.slack.com/services/new/bot and put the token  
+    token: '<your api key>', // Add a bot https://my.slack.com/services/new/bot and put the token  
     name: 'Dont Here Me'
-    
 });
 
-
-
-bot.on('message', function(data) {
-
-    var messageText=data.text
- console.log('im in')
-
-    if(messageText.indexOf('@channel') > -1 === true || messageText.indexOf('@here') > -1  === true){
-       var params = {
-        icon_emoji: ':rage3:'
-    };
-    
-
-    bot.postMessageToChannel('general', "Hey ! Pls don't use 'HERE' or 'CHANNEL' unnecessarily, Remember you just pinged everybody in this group" , params);
+bot.on('message', function (data) {
+    var messageText = data.text
+    if (messageText.indexOf('@channel') > -1 === true || messageText.indexOf('@here') > -1 === true) {
+        var params = {
+            icon_emoji: ':rage3:'
+        };
+        bot.postMessageToChannel('general', "Hey ! Pls don't use 'HERE' or 'CHANNEL' unnecessarily, Remember you just pinged everybody in this group", params);
     }
-    
-    
 });
